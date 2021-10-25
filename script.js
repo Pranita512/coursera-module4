84484c5bcc6a78d814792f9bccf50b1c84066849
Pranita512 <91153178+Pranita512@users.noreply.github.com>
2021-10-25 17:13:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+(function () {
+
+    var names = ["Pranita", "John", "Jen",  "Anagha"];
+    
+    for (var i = 0; i < names.length; i++) {
+      var firstLetter = names[i].charAt(0).toLowerCase();
+    
+      if (firstLetter === 'j') {
+        byeSpeaker(names[i]);
+      }
+      else {
+        helloSpeaker(names[i]);
+      }
+    }
+    
+    })();
+    
+    
+   
+    
